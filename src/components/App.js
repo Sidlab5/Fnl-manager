65fb285bfe1fd015de14d810cec1d97ca88de4cc
abdullah-adel <abboody19963@gmail.com>
2021-09-28 20:20:09 +0300

    ADDED: Initial setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
+import { Button } from 'antd';
 import React from 'react'
 
 const App = () => {
 	return (
 		<div className='app'>
 			<h1>React Electron Boilerplate</h1>
+			<Button>test</Button>
 			<p>This is a simple boilerplate for using React with Electron</p>
 		</div>
 	)
 }
 
-export default App
+export default App;
